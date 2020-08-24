cbbd196946cec31891fbe9fdd89bebd950fdbf43
Andres Carrillo <andresjoe24@gmail.com>
2020-08-23 23:50:12 -0500

    Create endpoint disable city

--- src/services/city.service.ts
+++ src/services/city.service.ts
@@ -1,5 +1,9 @@
 import { City } from "../models/city.entity";
 import { getRepository } from "typeorm";
+import { ServiceEntity } from '../models/service.entity';
+import fp from 'lodash/fp';
+import _ from 'lodash';
+import { HttpResponse } from '../libs/httpResponse';
 
 class CityService {
   async getAllCities() {
@@ -13,6 +17,25 @@ class CityService {
     const newCity = cityRepository.create({ name });
     return await newCity.save();
   }
+
+  async disableCity( id: number ){
+
+        const httpResponse = new HttpResponse();
+
+        if(!_.isNaN(id)){
+            
+            const cityRepository = getRepository(City);
+            const cityToDisable = await cityRepository.findOne(id);
+    
+            httpResponse.errorNotFoundID('City', id)
+            return httpResponse;
+        }
+
+        httpResponse.errorFormatInvalid(id);
+        return httpResponse;
+
+    }
+
 }
 
 const cityService = new CityService();
